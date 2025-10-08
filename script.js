47f3e85e6664746e14615dd8b1bf1f73d03a7379
Shivam Chaurasiya <shivam825825@gmail.com>
2025-10-09 00:42:01 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const apikey="4d46c115eff49bda6bb20b033d5066d9";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox=document.querySelector(".search input");
+const searchButton=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response=await fetch(apiurl + city + `&appid=${apikey}`);
+    
+
+    if(response.status === 404) {
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+
+    }else{
+        var data = await response.json();
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity +"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed +"km/h";
+    
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src="./clouds.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            weatherIcon.src="./clear.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            weatherIcon.src="./rain.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            weatherIcon.src="./drizzle.png";
+        }
+        else if(data.weather[0].main=="Mist"){
+            weatherIcon.src="./mist.png";
+        }
+    
+        document.querySelector(".weather").style.display="block";
+        document.querySelector(".error").style.display="none";
+    }
+}
+
+function handleSearch() {
+    if(searchBox.value.trim() !== "") {
+        checkWeather(searchBox.value);
+    }
+}
+
+
+searchButton.addEventListener("click", handleSearch);
+
+
+searchBox.addEventListener("keypress", (event) => {
+    if(event.key === "Enter") {
+        handleSearch();
+    }
+});
+
